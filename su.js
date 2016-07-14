e2ddd22f375f9331725c525f2e5871db472bc19b
mamuya01 <01mbvi@gmail.com>
2016-07-14 15:33:44 +0300

    initial commit

---
+++ su.js
@@ -0,0 +1,11 @@
+
+document.getElementById("copyRight").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " SUA Insurance.</p>";
+
+document.getElementById("navSUA").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='login.html'>HOME</a></li>"  +
+"<li><a href='about.html'>ABOUT US</a></li>" +
+"</ul>"; 
+ 
+
